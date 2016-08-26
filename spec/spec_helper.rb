92517b33be76108f6044efde839aa7c754f3c89a
Johan Lyheden <Johan.Lyheden@UNIBET.com>
2016-08-26 15:36:39 +0200

    Add some more required facts for puppet 4

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
   c.default_facts = {
-    :kernel => 'Linux'
+    :kernel => 'Linux',
+    :operatingsystem => 'RedHat',
+    :operatingsystemmajrelease => '6',
   }
 end
